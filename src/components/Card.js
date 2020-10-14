89e36f0843f0614aa82b5172b192055d1b572d7c
Marina Pisareva <aniram1s@yandex.ru>
2020-10-14 22:36:26 +0300

    средняя температура

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,26 +1,21 @@
 import React from 'react';
-const moment = require('moment')
-moment.locale('ru');
+import moment from 'moment';
+import russianLocale from "moment/locale/ru";
+moment.updateLocale('ru', russianLocale);
 
 export default class Card extends React.Component {
   render() {
     
-    let newDate = new Date();
-    const weekDay = this.props.day.dt * 1000
-    newDate.setTime(weekDay)
-
-    const logoURL = `owf owf-${this.props.day.weather[0].id} owf-5x`
-
     return (
       <div className='col-auto'>
         <div className='card bg-light'>
-          <h3 className='card-title'>{moment(newDate).format('dddd')}</h3>
-          <p className="text-muted">{moment(newDate).format('MMMM Do, h:mm a')}</p>
-          <i className={logoURL}></i>
-          <h2>{Math.round(this.props.day.main.temp)} °C</h2>
+          <h3 className='card-title'>{}</h3>
+          <p className="text-muted">{this.props.day.day}</p>
+
+          <h2>{this.props.day.avgTemp} °C</h2>
           <div className='card-body'>
             <button className='btn btn-dark btn-outline-light'>
-              {this.props.day.weather[0].description}
+              {/* {this.props.day.weather[0].description} */}
             </button>
           </div>
         </div>
--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -1,35 +1,56 @@
 import React from 'react';
-import Card from './Card'
+import Card from './Card';
 
 export default class Forecast extends React.Component {
-state = {
-    days: []
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      days: [],
+    };
+  }
 
-componentDidMount = () => {
-    fetch("https://api.openweathermap.org/data/2.5/forecast?q=Rostov-on-Don&lang=ru&units=metric&APPID=5f125c81f099c4c9663f7b46c8e4d885")
-    .then(res => res.json())
-    .then(data => {
-        const dailyData = data.list.filter(reading => reading.dt_txt.includes("18:00:00"))
-        this.setState({days: dailyData})})
-    .catch((err) => {
-        console.log(err);
+  componentDidMount = () => {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/forecast?q=Rostov-on-Don&lang=ru&units=metric&APPID=5f125c81f099c4c9663f7b46c8e4d885'
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        const newData = data.list.reduce((acc, item) => {
+          const day = item.dt_txt.split(' ')[0]; // Дата как ключ
+          if (!acc[day]) {
+            // если у нас нет такого ключа, то создаем
+            acc[day] = [];
+          }
+          acc[day].push(item.main.temp); // добавляем температуру
+          return acc;
+        }, {});
+
+        const temp = [];
+        for (let item in newData) {
+          const avgTemp = Math.round(
+            newData[item].reduce((acc, cur) => {
+              return acc + cur;
+            }, 0) / newData[item].length
+          );
+          temp.push({
+            day: item,
+            avgTemp: avgTemp,
+          });
+        }
+        this.setState({ days: temp });
       });
-}
-    formatCards = () => {
-        return this.state.days.map((day, i) => <Card day={day} key={i}/>)
-    }
+  };
+  renderCards = () => {
+    return this.state.days.map((day, i) => <Card day={day} key={i} />);
+  };
 
-    render () {
-        return (
-            <div className="container">
-            <h1 className="display-5 jumbotron">Прогноз погоды на 5 дней</h1>
-      <h5 className="display-7 text-muted">Ростов-на-Дону</h5>
-        <div className="row justify-content-center">
-          {this.formatCards()}
-
-        </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className='container'>
+        <h1 className='display-5 jumbotron'>Прогноз погоды</h1>
+        <h5 className='display-7 text-muted'>Ростов-на-Дону</h5>
+        <div className='row justify-content-center'>{this.renderCards()}</div>
+      </div>
+    );
+  }
 }
